4564233f26519f6d439ee7e565e4f2a9b8ae3dba
Puneet <puneettiwari61@gmail.com>
2020-03-10 18:04:50 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// You js goes here
+  alert('hello I m extension of chrome ' + window.location.href)
+
+
+  var issues = document.querySelectorAll('.link-gray-dark')
+  issues =  Array.from(issues)
+  issues.shift()
+  issues.forEach(i => {
+    i.innerText = i.innerText +  " Project"
+  })
+  console.log(issues)
+
+  // content.js
+// background.js
+
+// // Called when the user clicks on the browser action.
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // Send a message to the active tab
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     var activeTab = tabs[0];
+//     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+//   });
+// });
+
+// // This block is new!
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     if( request.message === "open_new_tab" ) {
+//       chrome.tabs.create({"url": request.url});
+//     }
+//   }
+// );
+
