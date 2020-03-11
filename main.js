e635d6ed3afbdd47bdfc1326f6926c321c908928
Ayush_Mann <learnertsk@gmail.com>
2020-03-11 18:39:31 +0530

    made the display of the innertext of the project page in console

--- main.js
+++ main.js
@@ -1,14 +1,55 @@
 // You js goes here
+// import axios from "axios"
+
   alert('hello I m extension of chrome ' + window.location.href)
 
+ 
+  // issues =  Array.from(issues)
+  // issues.shift()
 
-  var issues = document.querySelectorAll('.link-gray-dark')
-  issues =  Array.from(issues)
-  issues.shift()
-  issues.forEach(i => {
-    i.innerText = i.innerText +  " Project"
-  })
-  console.log(issues)
+  // issues.forEach(i => {
+  //   i.innerText = i.innerText +  " Project" 
+  // })
+  // console.log(issues,"sadasdsa1231231")
+  // console.log(result, "resyusds")
+  // let project = document.querySelector('.f6 text-bold link-gray-dark no-underline')
+ 
+  // let result = project.unshift()
+  // console.log(result,"sdasdsas")
+
+// fetch("https://github.com/repos/ayush-mann/trello-fullStack/issues/1",{
+//   method:"GET",
+//   headers:{
+//     "Content-Type":"text",
+//     "Accept":"application/vnd.github.machine-man-preview",
+//     // mode: 'cors',
+//     // credentials: 'omit'
+    
+//   }
+// }).then(res => res.text)
+// .then(res => console.log(res))
+
+
+fetch("https://github.com/ayush-mann/trello-fullStack/issues/1").then(function (response) {
+	// The API call was successful!
+	return response.text()
+}).then(function (html) {
+	// This is the HTML from our response as a text string
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(html, 'text/html');
+  
+  var item = doc.querySelectorAll('.js-issue-sidebar-form');
+  let children = item[1].innerText
+  let regexElem  = children.replace(/ /g,'').trim().replace(/\r?\n|\r/g," ").split('    ')
+  let filtered = regexElem.filter(item => item.length>0)
+  filtered.shift()
+  console.log(filtered.join(" "))  
+  var issue = document.getElementById(`issue_7_link`)
+console.log(issue,"isuue gol,mal")
+issue.innerText = issue.innerText + " " + `Project ${filtered[1]}`
+ 
+}).catch(err => console.log(err))
+
 
   // content.js
 // background.js
